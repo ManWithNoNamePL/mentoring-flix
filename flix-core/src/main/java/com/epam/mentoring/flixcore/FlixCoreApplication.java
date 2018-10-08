b71d08fc400a3a76bcabec9f2114ffd59fce4f44
Maxim Peters <maxim_peters@epam.com>
2018-10-08 14:08:40 +0200

    Created flix-core project

---
+++ flix-core/src/main/java/com/epam/mentoring/flixcore/FlixCoreApplication.java
@@ -0,0 +1,12 @@
+package com.epam.mentoring.flixcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixCoreApplication.class, args);
+	}
+}
